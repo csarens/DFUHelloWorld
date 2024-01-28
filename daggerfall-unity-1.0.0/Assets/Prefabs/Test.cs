2d72e586e13057ee4ddcddfbb365a3e66a6403eb
Jack Tomlinson <jackotomlinson1@googlemail.com>
2024-01-28 22:50:43 +0000

    register to window change event to determine if in inventory

--- daggerfall-unity-1.0.0/Assets/Prefabs/Test.cs
+++ daggerfall-unity-1.0.0/Assets/Prefabs/Test.cs
@@ -1,30 +1,26 @@
 using DaggerfallWorkshop.Game;
 using DaggerfallWorkshop.Game.Items;
 using DaggerfallWorkshop.Game.UserInterfaceWindows;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    private DaggerfallUI UI;
-    private DaggerfallInventoryWindow _window;
     // Start is called before the first frame update
     void Start()
     {
-
-        UI = GetComponent<DaggerfallUI>();
-        _window = UI.InventoryWindow;
-        _window.OnItemHover += (e, b) =>
+        DaggerfallUI.UIManager.OnWindowChange += (e,b) =>
         {
-            Debug.Log($"{e.weightInKg} and {e.value} and {(WeaponMaterialTypes)e.NativeMaterialValue}");
+            if (DaggerfallUI.UIManager.TopWindow is DaggerfallInventoryWindow window)
+            {
+                window.OnItemHover += (c, d) => Debug.Log($"{c.weightInKg} and {c.value} and {(WeaponMaterialTypes)c.NativeMaterialValue}");  
+            }
         };
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+ 
     }
 }
